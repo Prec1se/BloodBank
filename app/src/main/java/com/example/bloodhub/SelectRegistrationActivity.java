2e641dcc33d98ae755cee500def542089dc36de8
Prec1se <mainulislam-2018325306@cs.du.ac.bd>
2022-11-02 00:12:28 +0600

    selection of registration or login page ui completed, added custom button, custom color

--- app/src/main/java/com/example/bloodhub/SelectRegistrationActivity.java
+++ app/src/main/java/com/example/bloodhub/SelectRegistrationActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class SelectRegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.activity_select_registration);
     }
 }
