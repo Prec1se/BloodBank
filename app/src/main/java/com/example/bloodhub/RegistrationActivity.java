49cf6efe8cc0e0eb1e5b7aff6007eb3d6e0ce1a1
Prec1se <mainulislam-2018325306@cs.du.ac.bd>
2022-11-02 23:18:07 +0600

    Registration activity code completed

--- app/src/main/java/com/example/bloodhub/RegistrationActivity.java
+++ app/src/main/java/com/example/bloodhub/RegistrationActivity.java
@@ -2,13 +2,27 @@ package com.example.bloodhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private TextView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bloodhub/SelectRegistrationActivity.java
+++ app/src/main/java/com/example/bloodhub/SelectRegistrationActivity.java
@@ -2,13 +2,39 @@ package com.example.bloodhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SelectRegistrationActivity extends AppCompatActivity {
+
+    private TextView backButton;
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_registration);
+
+        backButton = findViewById(R.id.backButton);
+        register = findViewById(R.id.registerButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectRegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectRegistrationActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
